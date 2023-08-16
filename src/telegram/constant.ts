e064a25b0b7ff97b78d8115fa51d0a9faee1a2fa
gbubemi22 <slimnfine22@gmail.com>
2023-08-16 18:46:50 +0100

    fix

--- src/telegram/constant.ts
+++ src/telegram/constant.ts
@@ -1,5 +1,5 @@
 export const CONSTANTS = {
-  phoneNumber: '+2347030762738',
-  api_id: '20691665',
-  api_hash: 'b89482cb24740bd49af5f20b70e92b3c',
+  phoneNumber: 'your phone number',
+  api_id: 'your api id',
+  api_hash: 'your api hash',
 };
